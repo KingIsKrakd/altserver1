9be02d9bc31ed8dc86fad5fc65b1b1d93b8ab83e
nab138 <56705177+nab138@users.noreply.github.com>
2022-10-26 10:50:52 -0400

    Update comments

--- main.py
+++ main.py
@@ -61,7 +61,6 @@ if(os.path.isdir("lib") == False):
 def resource_path(relative_path):
 	""" Get absolute path to resource, works for dev and for PyInstaller """
 	try:
-		# PyInstaller creates a temp folder and stores path in _MEIPASS
 		base_path = sys._MEIPASS
 	except Exception:
 		base_path = os.path.abspath(".")
